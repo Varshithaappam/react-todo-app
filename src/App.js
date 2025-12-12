30fd55a43a21bdf345d7262bf34124b6e288a18b
Varshithaappam <162917599+Varshithaappam@users.noreply.github.com>
2025-12-12 11:21:44 +0530

    Refactor App component to simplify todos state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React,{ useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
@@ -6,37 +6,31 @@ import TodoList from './components/Todolist';
 
 function App() {
   const [input, setInput] = useState("");
-
-  // Load todos from localStorage on initial render
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem("todos");
-    return savedTodos ? JSON.parse(savedTodos) : [];
-  });
-
-  // Save todos to localStorage whenever they change
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
+  const [todos, setTodos] = useState([]);
   return (
-    <div className='container'>
-      <div className='app-wrapper'>
-        <Header />
-
+  <div className='container'>
+    <div className='app-wrapper'>
+      <div>
+        <Header/>
+      </div>
+      <div>
         <Form 
-          input={input}
-          setInput={setInput}
-          todos={todos}
-          setTodos={setTodos}
+        input ={input}
+        setInput = {setInput}
+        todos = {todos}
+        setTodos = {setTodos}
         /> 
-
+      </div>
+      <div>
         <TodoList 
-          todos={todos}
-          setTodos={setTodos}
+        todos={todos}
+        setTodos={setTodos}
         />
       </div>
+    </div>
+
     </div>
   );
 }
 
-export default App;
+export default App
